909a531489f65b7c4de10db2d70c18a664e244fa
CalvinE <calvin.echols@gmail.com>
2023-04-16 22:47:16 -0400

    playing with template

--- cmd/bulkrename.go
+++ cmd/bulkrename.go
@@ -4,15 +4,16 @@ import (
 	"errors"
 	"fmt"
 	"regexp"
+	"text/template"
 
 	"github.com/spf13/cobra"
 )
 
 type bulkRenameArgs struct {
-	RootPath               string
-	TargetRegexString      string
-	DestinationRegexString string
-	Recursive              bool
+	RootPath                  string
+	TargetRegexString         string
+	DestinationTemplateString string
+	Recursive                 bool
 }
 
 var bulkRenameCommand = &cobra.Command{
@@ -29,7 +30,7 @@ func bulkRenameInit() {
 	rootCmd.AddCommand(bulkRenameCommand)
 	bulkRenameCommand.PersistentFlags().StringVarP(&bkrnArgs.RootPath, "rootPath", "p", "", "The root path to perform the bulk rename in")
 	bulkRenameCommand.PersistentFlags().StringVarP(&bkrnArgs.TargetRegexString, "targetRegex", "t", "", "the target regex to use for renaming with named capture groups present in the destination regex")
-	bulkRenameCommand.PersistentFlags().StringVarP(&bkrnArgs.DestinationRegexString, "destinationRegex", "d", "", "the destination regex to use for renaming with named capture groups present in the target regex")
+	bulkRenameCommand.PersistentFlags().StringVarP(&bkrnArgs.DestinationTemplateString, "destinationTemplate", "d", "", "the destination template to use for renaming with named capture groups present in the target regex")
 	bulkRenameCommand.PersistentFlags().BoolVarP(&bkrnArgs.Recursive, "recursive", "r", false, "if present the bulk rename will work recursively")
 }
 
@@ -51,31 +52,33 @@ func bulkRenameRun(cmd *cobra.Command, args []string) error {
 		return errors.New("no named capture groups found in target regex")
 	}
 	// validate destination regex
-	if len(bkrnArgs.DestinationRegexString) == 0 {
-		return errors.New("destination regex not provided")
+	if len(bkrnArgs.DestinationTemplateString) == 0 {
+		return errors.New("destination template not provided")
 	}
-	destinationRegex, err := regexp.Compile(bkrnArgs.DestinationRegexString)
+	// using .Option("missingkey=error") so that if a named capture group from the regex is missing in the template we should get an error?
+	// want to think on this more... Id like to find a way to know ahead of running the template
+	// it looks like the template struct contains a lot of data we can possibly use to find the variable names used in the template
+	// need to look more into it, but NodeAction (1) nodes in the tree can be navigated and within them there are Pipe.Cmd.Args that
+	// contain NodeField(8) that hold the value of the variable name in the template...
+	t, err := template.New("filePart").Option("missingkey=error").Parse(bkrnArgs.DestinationTemplateString)
 	if err != nil {
-		return fmt.Errorf("destination regex failed to compile: %v", err)
+		return errors.New("failed to parse destination template")
 	}
-	destinationCaptureGroupNames := destinationRegex.SubexpNames()
-	if len(destinationCaptureGroupNames) <= 1 {
-		return errors.New("no named capture groups found in destination regex")
-	}
+	fmt.Println(t)
 	// we need to make sure for each named destination capture group we have a comparable named capture group in the target regex
-	for i := 0; i < len(destinationCaptureGroupNames); i++ {
-		destName := destinationCaptureGroupNames[i]
-		found := false
-		for j := 0; j < len(targetCaptureGroupNames); j++ {
-			if destName == targetCaptureGroupNames[j] {
-				found = true
-				break
-			}
-		}
-		if !found {
-			return fmt.Errorf("target regex is missing named capture group found in destination regex: %s", destName)
-		}
-	}
+	// for i := 0; i < len(destinationCaptureGroupNames); i++ {
+	// 	destName := destinationCaptureGroupNames[i]
+	// 	found := false
+	// 	for j := 0; j < len(targetCaptureGroupNames); j++ {
+	// 		if destName == targetCaptureGroupNames[j] {
+	// 			found = true
+	// 			break
+	// 		}
+	// 	}
+	// 	if !found {
+	// 		return fmt.Errorf("target regex is missing named capture group found in destination regex: %s", destName)
+	// 	}
+	// }
 
 	fmt.Println(bkrnArgs)
 	return nil
