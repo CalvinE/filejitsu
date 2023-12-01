83ea8d5ca2ae0e4c0e8dff2d6353b0438ad1ce61
CalvinE <calvin.echols@gmail.com>
2023-12-01 16:31:27 -0500

    typo fix

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ COMMIT_HASH=$(git rev-parse --short HEAD)
 BUILD_DATE=$(date '+%Y%m%d')
 BUILD_TAG=$(git describe --tags $COMMIT_HASH)
 
-echo "building filejitsu version: Hash=$COMMIT_HASH Date=$BUILD_DATE TAG=$BUILD_TAG"
+echo "building filejitsu version: Hash=$COMMIT_HASH Date=$BUILD_DATE Tag=$BUILD_TAG"
 
 go build -ldflags="-X main.commitHash=$COMMIT_HASH -X main.buildDate=$BUILD_DATE -X main.buildTag=$BUILD_TAG" -o "filejitsu" .
 
